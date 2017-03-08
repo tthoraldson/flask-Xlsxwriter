f075d61084e1896fe094779f07f590e00fbeab57
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-03-08 10:34:49 -0600

    first commit

---
+++ test.py
@@ -0,0 +1,22 @@
+# (c) Theresa Thoraldson 2017 | http://github.com/tthoraldson
+# theresa.thoraldson@gmail.com
+
+import sys
+from flask import Flask
+import xlsxwriter
+
+# Define Flask Server
+app = Flask('_test_')
+
+
+# setting up workbook and worksheet
+workbook = xlsxwriter.Workbook('test.xlsx')
+worksheet = workbook.add_worksheet()
+
+@app.route('/')
+def index():
+    return 'App Running!'
+
+@app.route
+    worksheet.write('A1', 'Hello world')
+    workbook.close()
